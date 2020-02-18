0c44b6e42292f44d21ec5d34b1526f2f99d2cf94
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2020-02-18 14:15:38 +0100

    add angular material

--- src/app/fight-arena/fight-arena.component.ts
+++ src/app/fight-arena/fight-arena.component.ts
@@ -37,12 +37,12 @@ export class FightArenaComponent implements OnInit {
 			}
 		} else {
 			if (this.Attacker._speed < this.Defender._speed) {
-				this.switchAttackerAndDefender()	
+				this.switchAttackerAndDefender()
 			}
 		}
 		return this.Attacker
   }
-  
+
   /**
    * l'attaquant devient défenseur, et le défenseur devient attaquant
    */
@@ -50,24 +50,24 @@ export class FightArenaComponent implements OnInit {
 		let temp: Pokemon
 		temp = this.Attacker
 		this.Attacker = this.Defender
-		this.Defender = temp	
+		this.Defender = temp
 	}
-  
+
   /**
    * calcul la valeur de l'attaque
    * @param attacker
    * @param defender
    * @param attack
-   * @return la valeur réel de l'attaque 
+   * @return la valeur réel de l'attaque
    */
   calculateAttackRealValue(attacker : Pokemon, defender: Pokemon, attack: Attack): number{
 		return Math.floor(Math.floor(Math.floor(2*attacker._level/5 + 2)*attacker._offensiveStat*attack._damage/defender._defensiveStat)/50)+2
   }
-  
+
   /**
    * modifie les pv d'un pokemon
-   * @param pokemon 
-   * @param value 
+   * @param pokemon
+   * @param value
    */
   impactAttackedPokemonHealth(pokemon: Pokemon, value: number){
 		pokemon.modifyHealth(value)
