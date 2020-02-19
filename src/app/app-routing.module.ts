00b5bf00c8ce54a9da40c49971ca0cde275f1b4a
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2020-02-19 23:06:02 +0100

    feat (menu): add menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ChoiceComponent } from './choice/choice.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DialoglaunchComponent } from './dialoglaunch/dialoglaunch.component';
@@ -13,8 +14,12 @@ const routes: Routes = [
     component: FightArenaComponent,
   },
   {
+    path: 'choise',
+    component: ChoiceComponent,
+  },
+  {
     path: '',
-    redirectTo: '/launch',
+    redirectTo: '/choise',
     pathMatch: 'full'
   }
 ];
